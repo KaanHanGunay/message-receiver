5e3cf49e1b1b24580e4629e768f93cf3d86bffcf
Kaan Han Günay <kaangunay16@icloud.com>
2025-03-03 11:36:43 +0300

    initial commit

---
+++ src/main/java/tr/com/kaanhangunay/examples/message_receiver/ExampleMessageListener.java
@@ -0,0 +1,22 @@
+package tr.com.kaanhangunay.examples.message_receiver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import tr.com.kaanhangunay.examples.messages.ExampleMessage;
+import tr.com.kaanhangunay.examples.models.GenericApplicationEvent;
+
+@Component
+public class ExampleMessageListener {
+
+  private static final Logger log = LoggerFactory.getLogger(ExampleMessageListener.class);
+
+  @EventListener(
+      condition =
+          "#message.body instanceof T(tr.com.kaanhangunay.examples.messages.ExampleMessage)")
+  public void onMessage(GenericApplicationEvent<ExampleMessage> message) {
+    ExampleMessage msg = message.body();
+    log.info(msg.getMessage());
+  }
+}
---
+++ src/main/java/tr/com/kaanhangunay/examples/message_receiver/MessageReceiverApplication.java
@@ -0,0 +1,13 @@
+package tr.com.kaanhangunay.examples.message_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageReceiverApplication.class, args);
+	}
+
+}
